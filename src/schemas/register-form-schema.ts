ad99e746c4afc962dac39520854017ac55c9c7af
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-07-24 22:16:21 +0700

    fix: change password validation for authentication to required only

--- src/schemas/register-form-schema.ts
+++ src/schemas/register-form-schema.ts
@@ -3,15 +3,7 @@ import { z } from "zod";
 export const RegisterFormSchema = z.object({
   email: z.email({ message: "email format is invalid" }).trim(),
   fullName: z.string().min(1, { message: "fullname is required" }).trim(),
-  password: z
-    .string()
-    .min(1, { message: "password is required" })
-    .regex(/[a-zA-Z]/, { message: "password should at least one letter." })
-    .regex(/[0-9]/, { message: "password should at least one number." })
-    .regex(/[^a-zA-Z0-9]/, {
-      message: "password should at least has one special character.",
-    })
-    .trim(),
+  password: z.string().min(1, { message: "password is required" }).trim(),
 });
 
 export type RegisterFormValues = z.infer<typeof RegisterFormSchema>;
