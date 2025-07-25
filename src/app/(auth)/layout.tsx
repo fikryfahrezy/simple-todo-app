7edf4c675864874b61b0edc4511bbf97b0556366
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-07-25 15:33:00 +0700

    feat: update the layout for auth group

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,7 @@
+import { getImageProps } from "next/image";
 import { redirect } from "next/navigation";
 import { getSession } from "@/lib/session";
+import { getBackgroundImage } from "@/lib/utils";
 import type { NodewaveServiceAuthzResponseBody } from "@/services/nodewave-service.types";
 
 export default async function AuthLayout({
@@ -16,5 +18,21 @@ export default async function AuthLayout({
     redirect("/admin");
   }
 
-  return children;
+  const backgroundShapeImageProps = getImageProps({
+    alt: "",
+    width: 500,
+    height: 128,
+    src: "/images/background-shape.png",
+  });
+  const backgroundShapeImageSrc = getBackgroundImage(
+    backgroundShapeImageProps.props.srcSet,
+  );
+  return (
+    <div
+      style={{ backgroundImage: backgroundShapeImageSrc }}
+      className='tw:w-full tw:h-dvh tw:pt-28 tw:px-4 tw:pb-4 tw:bg-neutral-50 tw:bg-no-repeat tw:bg-size-[100%_50%] tw:lg:pt-[170px]'
+    >
+      <div className='tw:max-w-[560px] tw:mx-auto'>{children}</div>
+    </div>
+  );
 }
