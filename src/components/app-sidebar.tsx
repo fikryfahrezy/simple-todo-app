368468c813c779aa322e1ebeb18d07381d9391b3
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-07-26 00:18:21 +0700

    feat: update sidebar component

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarRail,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { HouseIcon } from "./icons";
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible='icon' {...props}>
+      <SidebarHeader>
+        <div className='tw:flex'>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton className='tw:text-sidebar-accent-foreground tw:text-xl tw:font-bold tw:pointer-events-none tw:opacity-0 tw:group-data-[state=expanded]:opacity-100'>
+                Nodewave
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+          <SidebarTrigger className='tw:group-data-[state=collapsed]:[&>svg]:rotate-180' />
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton asChild isActive>
+                <Link href='/admin'>
+                  <HouseIcon className='tw:size-4' />
+                  <span>To do</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
