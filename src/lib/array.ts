276769b93f2a1be658020191faf501bc7bfc1540
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-07-26 11:58:12 +0700

    fix: `range` function to return range instead empty array when start is equal end

--- src/lib/array.ts
+++ src/lib/array.ts
@@ -1,6 +1,6 @@
 // Ref: https://github.com/themesberg/flowbite-react/blob/38913e51536ecf1c8e924a6de571880cb91d2ea0/packages/ui/src/components/Pagination/Pagination.tsx
 export function range(start: number, end: number): number[] {
-  if (start >= end) {
+  if (start > end) {
     return [];
   }
 
