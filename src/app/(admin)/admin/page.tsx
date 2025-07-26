c93c5308e4b85d2ac51aa9c569731079dd5c583e
Fikry Fahrezy <55774934+fikryfahrezy@users.noreply.github.com>
2025-07-26 08:30:46 +0700

    feat: integrate admin page with the API

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { toast } from "sonner";
 import { DropdownIcon, MagnifierIcon } from "@/components/icons";
-import { TypographyH2 } from "@/components/typograhpy";
+import { TypographyH2, TypographyUnorderedList } from "@/components/typograhpy";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
@@ -23,86 +26,202 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { useSession } from "@/hooks/use-session";
 import { range } from "@/lib/array";
+import { useGetAllTodos } from "@/queris/todo-query";
+import type { NodewaveServiceAuthzResponseBody } from "@/services/nodewave-service.types";
+
+const AVAILABLE_STATUSES = [
+  { label: "Done", value: "true" },
+  { label: "Undone", value: "false" },
+];
+
+const PAGE_PARAM_NAME = "page";
+const SEARCH_PARAM_NAME = "search";
+const STATUS_PARAM_NAME = "status";
 
 export default function AdminPage() {
-  const currentPage = 1;
+  const session = useSession<NodewaveServiceAuthzResponseBody>();
+
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const currentPageQuery = Number(searchParams.get(PAGE_PARAM_NAME)) || 1;
+  const searchQuery = searchParams.get(SEARCH_PARAM_NAME) || undefined;
+  const statusQuery = searchParams.get(STATUS_PARAM_NAME) || undefined;
+
   const totalPages = 20;
-  const lastPage = Math.min(Math.max(currentPage + 2, 3), totalPages);
+  const lastPage = Math.min(Math.max(currentPageQuery + 2, 5), totalPages);
   const firstPage = Math.max(1, lastPage - 4);
+
+  const getAllTodos = useGetAllTodos({
+    token: session.token,
+    params: {
+      page: currentPageQuery,
+      rows: 10,
+      filters: {
+        isDone: statusQuery ? statusQuery === "true" : undefined,
+        item: searchQuery,
+      },
+    },
+  });
+
+  const handleSearch = (term: string) => {
+    const params = new URLSearchParams(searchParams ?? undefined);
+    params.set(PAGE_PARAM_NAME, "1");
+    if (term) {
+      params.set(SEARCH_PARAM_NAME, term);
+    } else {
+      params.delete(SEARCH_PARAM_NAME);
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  const handleFilterChange = (key: string, value: unknown) => {
+    const params = new URLSearchParams(searchParams ?? undefined);
+    params.set(key, String(value));
+    replace(`${pathname}?${params.toString()}`);
+  };
+
+  const onSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const formObject = Object.fromEntries(formData.entries());
+    const searchInput = String(formObject[SEARCH_PARAM_NAME]);
+    handleSearch(searchInput);
+  };
+
+  const goToPreviousPage = (): void => {
+    handleFilterChange(PAGE_PARAM_NAME, Math.max(currentPageQuery - 1, 1));
+  };
+
+  const goToNextPage = (): void => {
+    handleFilterChange(
+      PAGE_PARAM_NAME,
+      Math.min(currentPageQuery + 1, totalPages),
+    );
+  };
+
+  const onStatusFilterChange = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+  ) => {
+    handleFilterChange(STATUS_PARAM_NAME, event.currentTarget.value);
+  };
+
+  useEffect(() => {
+    if (!getAllTodos.error) {
+      return;
+    }
+
+    toast.error(getAllTodos.error.message, {
+      description: (
+        <TypographyUnorderedList>
+          {getAllTodos?.error?.errors?.map((error) => {
+            return <li key={error}>{error}</li>;
+          })}
+        </TypographyUnorderedList>
+      ),
+    });
+  }, [getAllTodos.error]);
 
   return (
     <main className='tw:flex tw:h-full tw:flex-col'>
       <TypographyH2 className='tw:text-neutral-700 tw:mb-8'>To Do</TypographyH2>
       <Card className='tw:overflow-y-auto'>
         <div className='tw:flex tw:gap-10'>
-          <div className='tw:flex tw:gap-3'>
+          <form className='tw:flex tw:gap-3' onSubmit={onSearchSubmit}>
             <InputRoot
               className='tw:w-fit'
               startIcon={<MagnifierIcon className='tw:text-neutral-500' />}
             >
-              <Input placeholder='Search' dimension='lg' border='bottom' />
+              <Input
+                placeholder='Search'
+                dimension='lg'
+                border='bottom'
+                name={SEARCH_PARAM_NAME}
+                defaultValue={searchQuery}
+              />
             </InputRoot>
             <Button>Search</Button>
-          </div>
+          </form>
           <SelectRoot
             className='tw:w-fit'
             endIcon={<DropdownIcon className='tw:size-4' />}
           >
-            <Select dimension='lg' border='bottom'>
+            <Select
+              dimension='lg'
+              border='bottom'
+              value={statusQuery}
+              onChange={onStatusFilterChange}
+            >
               <option value=''>Filter by Status</option>
-              <option value='indonesia'>Indonesia</option>
+              {AVAILABLE_STATUSES.map(({ label, value }) => {
+                return (
+                  <option key={value} value={value}>
+                    {label}
+                  </option>
+                );
+              })}
             </Select>
           </SelectRoot>
         </div>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Name</TableHead>
-              <TableHead>To do</TableHead>
-              <TableHead>Status</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell className='tw:w-1/3'>Ahmad Akbar</TableCell>
-              <TableCell className='tw:w-1/3'>This</TableCell>
-              <TableCell className='tw:w-1/3'>
-                <Badge size='lg' variant='success'>
-                  Success
-                </Badge>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className='tw:w-1/3'>Ahmad Akbar</TableCell>
-              <TableCell className='tw:w-1/3'>Hello</TableCell>
-              <TableCell className='tw:w-1/3'>
-                <Badge size='lg' variant='destructive'>
-                  Pending
-                </Badge>
-              </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell className='tw:w-1/3'>Ahmad Akbar</TableCell>
-              <TableCell className='tw:w-1/3'>Good</TableCell>
-              <TableCell className='tw:w-1/3'>
-                <Badge size='lg' variant='success'>
-                  Success
-                </Badge>
-              </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+        {getAllTodos.isPending ? (
+          <p className='tw:text-center'>Loading...</p>
+        ) : getAllTodos.isError ? (
+          <p className='tw:text-center tw:text-destructive'>
+            Error: {getAllTodos.error?.message}
+          </p>
+        ) : getAllTodos.data?.content?.entries?.length === 0 ? (
+          <p className='tw:text-center'>Empty</p>
+        ) : (
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Name</TableHead>
+                <TableHead>To do</TableHead>
+                <TableHead>Status</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {getAllTodos.data.content.entries.map((todoItem) => {
+                return (
+                  <TableRow key={todoItem.id}>
+                    <TableCell className='tw:w-1/3'>
+                      {todoItem.userId}
+                    </TableCell>
+                    <TableCell className='tw:w-1/3'>{todoItem.item}</TableCell>
+                    <TableCell className='tw:w-1/3'>
+                      <Badge
+                        size='lg'
+                        variant={todoItem.isDone ? "success" : "destructive"}
+                      >
+                        {todoItem.isDone ? "Success" : "Pending"}
+                      </Badge>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        )}
+
         <Pagination>
           <PaginationContent className='tw:ml-auto'>
             <PaginationItem>
-              <PaginationPrevious />
+              <PaginationPrevious onClick={goToPreviousPage} />
             </PaginationItem>
             {range(firstPage, lastPage).map((page) => {
-              const isActive = page === currentPage;
+              const isActive = page === currentPageQuery;
               return (
                 <PaginationItem key={page}>
-                  <PaginationButton isActive={isActive}>
+                  <PaginationButton
+                    isActive={isActive}
+                    onClick={() => {
+                      handleFilterChange(PAGE_PARAM_NAME, page);
+                    }}
+                  >
                     {page}
                   </PaginationButton>
                 </PaginationItem>
@@ -110,7 +229,7 @@ export default function AdminPage() {
             })}
 
             <PaginationItem>
-              <PaginationNext />
+              <PaginationNext onClick={goToNextPage} />
             </PaginationItem>
           </PaginationContent>
         </Pagination>
